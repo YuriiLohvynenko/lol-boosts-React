32ace0eb5b670efac7cbbf12cc8f59caac520475
YuriiLohvynenko <polarbearbusiness@hotmail.com>
2024-08-06 05:51:35 -0700

    fix: cover image color

--- src/pages/home/hero.tsx
+++ src/pages/home/hero.tsx
@@ -1,10 +1,10 @@
 const Hero = () => {
   return (
     <>
-      <div className="absolute top-0 left-0 w-full z-0">
+      <div className="absolute top-0 left-0 w-full z-0 bg-[#0f1a31]">
         <img
           src="/images/cover/5.jpeg"
-          className="w-full blur-sm opacity-75"
+          className="w-full blur-sm opacity-30"
           alt="HERO"
         />
       </div>
