ba8c6e8d8049d662f61a45d945acb1b32a5596ee
YuriiLohvynenko <polarbearbusiness@hotmail.com>
2024-09-09 17:00:09 +0900

    fix: issue

--- src/pages/home/products.tsx
+++ src/pages/home/products.tsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import products from "../../data/products.json";
 import classNames from "../../consts/classNames";
 import { FaStar } from "react-icons/fa";
+import { Link } from "react-router-dom";
 const Products = () => {
   useEffect(() => {}, []);
   return (
@@ -40,14 +41,14 @@ const Products = () => {
       <div className="grid grid-cols-2 lg:max-w-[768px] mx-auto md:grid-cols-3 lg:grid-cols-3 gap-4 mt-6 lg:mt-10">
         {products.map((d: any, index: number) => {
           return (
-            <div key={index} className=" hover:scale-105 cursor-pointer group">
+            <Link key={index} to={d.link} className=" hover:scale-105 cursor-pointer group">
               <img
                 src={d?.url}
                 alt={d.title}
                 className="child w-full rounded-lg group-hover:border-[#2d6df7] group-hover:border-2"
               />
               <div className="mt-4 text-xl font-bold">{d.title}</div>
-            </div>
+            </Link>
           );
         })}
       </div>
